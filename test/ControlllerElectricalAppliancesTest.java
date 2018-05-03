27162d05960c46f1d8cb9c2039729b9db329c0df
Vlad <vladkozachek90@gmail.com>
2018-05-03 11:10:49 +0300

    редактирование тестов

--- test/ControlllerElectricalAppliancesTest.java
+++ test/ControlllerElectricalAppliancesTest.java
@@ -2,6 +2,8 @@ import controller.ControlllerElectricalAppliances;
 import model.ElectricalAppliance;
 import model.Kettle;
 import model.TV;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,19 +12,20 @@ import java.util.ArrayList;
 
 class ControlllerElectricalAppliancesTest {
 
+
+    @Rule
+    Kettle kettle = new Kettle(3, true, 2000);
+    TV tv = new TV(4, false, 500);
+    ControlllerElectricalAppliances controlllerElectricalAppliances = new ControlllerElectricalAppliances(); //добавляем приборы в квартиру
+
     @Test
     void addElectricalApplianceTest() { //проверка создался ли объект
-        Kettle kettle = new Kettle(3, true, 2000);
-        ControlllerElectricalAppliances controlllerElectricalAppliances = new ControlllerElectricalAppliances(); //добавляем приборы в квартиру
         controlllerElectricalAppliances.addElectricalAppliance(kettle);
         assertTrue(controlllerElectricalAppliances != null);
-    }
+       }
 
     @Test
     void powerConsumptionTest() { //проверяем правильно ли считает потребляемую мощность
-        Kettle kettle = new Kettle(3, true, 2000);  //создаем приборы
-        TV tv = new TV(4, false, 500);
-        ControlllerElectricalAppliances controlllerElectricalAppliances = new ControlllerElectricalAppliances();
         controlllerElectricalAppliances.addElectricalAppliance(kettle);
         controlllerElectricalAppliances.addElectricalAppliance(tv);
         int n = controlllerElectricalAppliances.powerConsumption();
@@ -31,23 +34,16 @@ class ControlllerElectricalAppliancesTest {
 
     @Test
     void searchTest() {  //проверяем находит ли метод элементы
-        Kettle kettle = new Kettle(3, true, 2000);
-        TV tv = new TV(4, false, 500);
-        ControlllerElectricalAppliances controlllerElectricalAppliances = new ControlllerElectricalAppliances();
         controlllerElectricalAppliances.addElectricalAppliance(kettle);
         controlllerElectricalAppliances.addElectricalAppliance(tv);
         ArrayList<ElectricalAppliance> n = controlllerElectricalAppliances.search(100, 800);
         for (ElectricalAppliance item : n) {
             assertEquals(tv, item);
-
         }
     }
 
     @Test
     void MetodSearchDiapazonTest() {
-        Kettle kettle = new Kettle(3, true, 2000);
-        TV tv = new TV(4, false, 500);
-        ControlllerElectricalAppliances controlllerElectricalAppliances = new ControlllerElectricalAppliances();
         controlllerElectricalAppliances.addElectricalAppliance(kettle);
         controlllerElectricalAppliances.addElectricalAppliance(tv);
         ArrayList<ElectricalAppliance> n =controlllerElectricalAppliances.search(-100,200);
